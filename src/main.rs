e5b307aecc5fa39b01f32ed63ae0e8ae47d9f798
Auke Oosterhoff <auke@orangetux.nl>
2022-08-27 12:06:10 +0200

    Something compiles!

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,56 @@
+use serde_json;
 mod call;
 mod call_result;
 
 fn main() {
-    route(on_heartbeat);
+    //route(on_heartbeat);
+    //parse(OCPP(call::Heartbeat{}))
+}
+
+trait Handler<B> {
+    fn call(self, msg: Request<B>);
+}
+
+#[derive(Debug)]
+struct OnHeartbeat {}
+
+impl<B> Handler<B> for OnHeartbeat
+where
+    B: std::fmt::Debug,
+{
+    fn call(self, msg: Request<B>) {
+        dbg!(msg);
+    }
+}
+
+struct OCPP<T>(pub T);
+
+#[derive(Debug)]
+struct Request<T> {
+    body: T,
+}
+
+#[derive(Debug)]
+struct Body {}
+
+fn parse(OCPP(message): OCPP<call::Heartbeat>) {
+    dbg!(message);
 }
 
 // A handling handling an OCPP Heartbeat request.
-fn on_heartbeat(_: call::Heartbeat) -> impl ToResponse {
+fn on_heartbeat(OCPP(data): OCPP<call::Heartbeat>) -> impl ToResponse {
     call_result::Heartbeat {
         current_time: chrono::Utc::now(),
     }
 }
 
-
-fn route<F, O>(handler: F) where 
-    F: Fn(call::Heartbeat) -> O,
-    O: ToResponse
-    {
-        dbg!(handler(call::Heartbeat {}).to_response());
+fn route<H, B>(handler: H)
+where
+    H: Handler<B>,
+{
+    let req: Request<&str> = Request { body: r#"{}"# };
+    let req: Request<Body> = Request { body: Body {} };
+    dbg!(OnHeartbeat{}.call(req));
 }
 
 /// Trait to parse the data section of an OCPP message to struct.
