af8c75c7d211ea55b90095744aaf344dd204f007
Auke Oosterhoff <auke@orangetux.nl>
2022-08-28 11:11:51 +0200

    Implement simple Handler for FnOnce()
    I started all over again. First, I'll try to implement a `Handler` trait
    and implement it for closures. Currently, a closure without arguments is
    supported only.

--- src/main.rs
+++ src/main.rs
@@ -3,57 +3,34 @@ mod call;
 mod call_result;
 
 fn main() {
-    //route(on_heartbeat);
-    //parse(OCPP(call::Heartbeat{}))
+    route(|| println!("On heartbeat"))
 }
 
-trait Handler<B> {
-    fn call(self, msg: Request<B>);
+trait Handler<In> {
+    fn call(self);
 }
 
-#[derive(Debug)]
-struct OnHeartbeat {}
-
-impl<B> Handler<B> for OnHeartbeat
-where
-    B: std::fmt::Debug,
-{
-    fn call(self, msg: Request<B>) {
-        dbg!(msg);
-    }
-}
-
-struct OCPP<T>(pub T);
-
-#[derive(Debug)]
 struct Request<T> {
     body: T,
 }
 
-#[derive(Debug)]
-struct Body {}
-
-fn parse(OCPP(message): OCPP<call::Heartbeat>) {
-    dbg!(message);
-}
-
-// A handling handling an OCPP Heartbeat request.
-fn on_heartbeat(OCPP(data): OCPP<call::Heartbeat>) -> impl ToResponse {
-    call_result::Heartbeat {
-        current_time: chrono::Utc::now(),
+impl<F> Handler<()> for F
+where
+    F: FnOnce(),
+{
+    fn call(self) {
+        println!("Handler for FnOnce()");
+        self()
     }
 }
 
-fn route<H, B>(handler: H)
+fn route<H, In>(handler: H)
 where
-    H: Handler<B>,
+    H: Handler<In>,
 {
-    let req: Request<&str> = Request { body: r#"{}"# };
-    let req: Request<Body> = Request { body: Body {} };
-    dbg!(OnHeartbeat{}.call(req));
+    handler.call();
 }
 
-/// Trait to parse the data section of an OCPP message to struct.
 trait FromRequest {
     fn from_request(_: &str) -> Self;
 }
